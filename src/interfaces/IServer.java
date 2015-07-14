712212d9c909858dc3b43a9adea3c3e69d966047
Mathieu <mat.boyer95@gmail.com>
2015-07-14 20:47:05 +0200

    Trame pour faire Serveur

---
+++ src/interfaces/IServer.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+
+
+public interface IServer {
+
+	void run();
+	boolean init();
+
+	
+}
--- src/server/DNSServer.java
+++ src/server/DNSServer.java
@@ -1,9 +1,62 @@
 package server;
 
-public class DNSServer {
 
+import interfaces.IServer;
+
+import java.awt.ItemSelectable;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class DNSServer implements IServer{
+
+	private Map<String, Map<String, String>> domains;
+	
 	public static void main(String[] args) {
 		// TODO Server
+	}
+
+	@Override
+	public void run() {
+		ServerSocket server = null;
+		Socket client = null;
+		
+		// Creation socket serveur  recuperer Workers  et Port 
+		
+		//TODO 
+		try {
+			server = new ServerSocket();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}//Définir le port avec config.ini
+		
+		while(true)
+		{
+			try {
+				client = server.accept();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+		
+		
+	}
+
+	@Override
+	public boolean init() {
+		// TODO définir les domaines .avec le config
+				domains = new HashMap<String, Map<String,String>>();
+				
+				
+				
+		return false;
 	}
 
 }
